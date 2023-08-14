27d0328ed9fd1ee55ba856c446ba92dbea2bf208
vasukapil2196 <vasukapil2196@gmail.com>
2023-08-14 18:58:29 +0530

    minor-fix

--- accordian/src/icons/accordianIcons.js
+++ accordian/src/icons/accordianIcons.js
@@ -3,7 +3,7 @@ import React from 'react';
 export const DropDownArrow = () => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
-    className="h-5 w-5"
+    className="h-8 w-8"
     viewBox="0 0 20 20"
     fill="currentColor"
   >
