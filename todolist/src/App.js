6c7acc9583c64ce733544383a47935c14e7c4134
vasukapil2196 <vasukapil2196@gmail.com>
2023-08-21 17:25:01 +0530

    todo

--- todolist/src/App.js
+++ todolist/src/App.js
@@ -1,13 +1,37 @@
-
-import './App.css';
-import Input from './components/Input';
-import List from './components/List';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const arr = ["play Cricket", "Football", "Hockey"];
+  const  [games,setGames] = useState(arr);
+  const [check,setCheck] = useState(Array(games.length).fill(false));
+  console.log(check);
+  const handleDelete = (index) => {
+    console.log(index,games[index])
+    const updatedArr = games.filter((el,idx)=>idx !== index);
+    console.log(updatedArr)
+    setGames(updatedArr)
+    setCheck(btn => btn.filter((_, idx) => idx !== index));
+  }
+
+  const handleChange = (id) => {
+    const updated = [...check]
+    check[id] = !check[id];
+    setCheck(btn => [...btn,updated])
+
+  }
   return (
     <div>
-     <Input/>
-     <List/>
+      {games.map((el, idx) => {
+        return (
+          <div className="flex items-center justify-center">
+        
+              <input type={"checkbox"} onChange={(e)=>{handleChange(idx)}}></input>
+            <p className="m-4">{el}</p>
+            {check[idx] && <button onClick={()=>{handleDelete(idx)}} className="border border-gray-500">Delete</button>}
+          </div>
+        );
+      })}
     </div>
   );
 }
